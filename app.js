6ebdff8be8d04c07788d5814ae346dc165dc79c6
KendraLynn <102690988+KendraLynn@users.noreply.github.com>
2023-04-04 13:51:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, world!');
+
+const port = 9000; // constant (used later)
+
+app.get('/', (req, res) => { // handler for the "/" path
+
+	res.send("hello world"); // sends a response and terminates the connection
 });
 
-app.listen(3000, () => {
-    console.log('App listening on port 9000!');
+app.get('/:msg/:text', (req, res) => { // handler for the "/" path
+	// req.params.msg, req.params.text
+	res.send(`message: ${req.params.msg}, text:${req.params.text}`); // sends a response and terminates the connection
 });
+
+app.listen(port, () => { console.log(`App running on port: ${port}`) });  // start listening for requests
